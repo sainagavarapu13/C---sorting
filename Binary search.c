7dd42c74f145f9baf2d85b167a1ad2272f06347d
sainagavarapu13 <sainagavarapu13@gmail.com>
2025-02-15 10:38:55 +0530

    Update and rename Binary search to Binary search.c

--- Binary search.c
+++ Binary search.c
@@ -10,17 +10,20 @@ int main() {
     low = 0;
     high = n - 1;
     middle = (low + high) / 2;
-
-    while (low <= high) {
-        if (arr[middle] < search)
+    while (low <= high) { //o(log n)
+	    // if ur search larger than middle then move ur low to mid+1;
+        if (arr[middle] < search) 
             low = middle + 1;
         else if (arr[middle] == search) {
             printf("%d",middle);
             break;
+	// search is samller than mid move ur high
         } else
             high = middle - 1;
         middle = (low + high) / 2;
     }
+// low and high crossed each other 
+	// elem not exist
     if (low > high)
         printf("Not found");
 }
